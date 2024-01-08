909c8f1ea8bb395287bf71b62ffe1af4fd0b0011
Juno Angela Zhang <53055796+juuunoz@users.noreply.github.com>
2024-01-08 13:23:09 -0500

    initial commit

---
+++ jriver-header.h
@@ -0,0 +1,54 @@
+#ifndef _JRIVER
+#define _JRIVER
+
+//define some constants
+#ifndef JRIVER-MAJORNUM
+#define JDRIVER-MAJORNUM 0  
+#endif
+
+#ifndef JRIVER-NUMDEVS
+#define JRIVER-NUMDEVS 1
+#endif
+
+#ifndef JRIVER-CHUNKSIZE
+#define JRIVER-CHUNKSIZE 4000
+#endif
+
+#ifndef JRIVER-CHUNKSETSIZE
+#define JRIVER-CHUNKSETSIZE 1000
+#endif
+
+#ifndef JDRIVER-BUFFERSIZE
+#define JDRIVER-BUFFERSIZE 4000
+#endif
+
+//device data structure
+struct chunkset 
+{
+	void **data;
+	struct chunkset *next;
+};
+
+//device informational structure
+struct jriver-device
+{
+	// data portion of struct
+	struct chunkset *data;
+	
+	//organizational portion of struct
+	int chunksize;
+	int numchunks;
+	
+	struct cdev chardev;
+	
+	long size;
+	
+	
+};
+
+int jriver-open(struct inode *inode, struct file *filp);
+int jriver-release(struct inode *inode, struct file *filp);
+ssize_t jriver-read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
+ssize_t jriver-write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
+
+#endif
---
+++ jriver.c
@@ -0,0 +1,29 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/fcntl.h>
+#include <linux/cdev.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <asm/uaccess.h>
+
+#include "jriver.h"
+
+ssize_t jriver-read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
+ssize_t jriver-write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
+int jriver-open(struct inode *inode, struct file *filp);
+int jriver-release(struct inode *inode, struct file *filp);
+
+struct file_operations jriver-fops = {
+	.owner =    THIS_MODULE,
+	.read =     jriver-read,
+	.write =    jriver-write,
+	.open =     jriver-open,
+	.release =  jriver-release,
+};
+
